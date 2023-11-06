c66c23f63deebf098cb9afd7570e8e0aeb2a666f
Roy01 <subhamroy1180@gmail.com>
2023-11-06 10:40:33 +0530

    optimize the prime no. file

--- src/PrintPrimeNumber.java
+++ src/PrintPrimeNumber.java
@@ -10,7 +10,7 @@ public class PrintPrimeNumber {
         if (n == 2) {
             System.out.println("n is Prime");
         } else {
-            for (int i = 2; i <= n - 1; i++) {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
                 if (n % i == 0) {
                     isPrime = false;
                 }

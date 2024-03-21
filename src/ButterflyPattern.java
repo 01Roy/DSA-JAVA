4db253f339c4868bfb1660c33ac33e057eda4c4b
roy01 <subhamroy1180@gmail.com>
2024-03-21 11:16:02 +0530

    pattern

--- src/ButterflyPattern.java
+++ src/ButterflyPattern.java
@@ -1,7 +1,7 @@
 public class ButterFlyPAttern {
     public static void main(String[] args) {
         int n = 4;
-
+        // FIRST HALF
         for (int i = 1; i <= n; i++) {
             // stars
             for (int j = 1; j <= i; j++) {
@@ -19,7 +19,7 @@ public class ButterFlyPAttern {
 
             System.out.println();
         }
-
+        // SECOND HALF
         for (int i = n; i >= 1; i--) {
             // stars
             for (int j = 1; j <= i; j++) {

e2234048315d29a32e4a5d1787dba8aa81344ce2
roy01 <subhamroy1180@gmail.com>
2024-04-16 12:02:32 +0530

    bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort {
+    public static void BubleSortAlgo(int arr[]) {
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tem = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = tem;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2, 8, 13, 24, 45, 0 };
+        BubleSortAlgo(arr);
+    }
+}
--- src/tempCodeRunnerFile.java
+++ src/tempCodeRunnerFile.java
@@ -1,1 +1,3 @@
-numbers
+for (int i = 0; i < arr.length; i++) {
+        // System.out.println(arr[i]);
+        // }
